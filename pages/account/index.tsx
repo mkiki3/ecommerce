3206f607efe7a802c7fa816fb2951af0f6d87603
mkiki3 <kikisymon3@gmail.com>
2022-02-24 17:38:20 -0600

    add components

---
+++ pages/account/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export const Account = () => {
+  return <>Account Page </>
+}
---
+++ pages/shoppingcart/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export const ShoppingCart = () => {
+  return <>ShoppingCart Page </>
+}
